d6fcb08387223a05fdc358493bf7b85702e076e2
daebon07 <muhammadshabran25@gmail.com>
2024-09-12 10:26:50 +0700

    cafe21.java

---
+++ cafe21.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class cafe21 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        boolean keanggotaan;
+        int jumlahkopi, jumlahteh, jumlahroti;
+        double hargakopi = 12000.0, hargateh= 7000.0, hargaroti= 20000.0, totalharga, nominalbayar;
+        float diskon= 10 / 100f;
+
+        System.out.print("Keanggotaan (true/false)");
+        keanggotaan = input.nextBoolean();
+        System.out.print("Jumlah pembelian kopi");
+        jumlahkopi = input.nextInt();
+        System.out.print("Jumlah pembelian teh");
+        jumlahteh = input.nextInt();
+        System.out.print("Jumlah pembelian roti");
+        jumlahroti = input.nextInt();
+
+
+        totalharga = (jumlahkopi*hargakopi) + (jumlahteh*hargateh) + (jumlahroti*hargaroti);
+        nominalbayar = totalharga - (diskon*totalharga);
+
+        System.out.println("keanggotaan" + keanggotaan);
+        System.out.println("Item Pembelian" + jumlahkopi + "kopi" + jumlahteh + "teh" + jumlahroti + "roti" );
+        System.out.println("Nominal Bayar" + nominalbayar);
+
+    }
+    
+}
