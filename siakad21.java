1faffedb2d81e0acecf74403137fe657479e9e84
daebon07 <muhammadshabran25@gmail.com>
2024-09-12 10:01:10 +0700

    jawaban pertanyaan 7

--- siakad21.java
+++ siakad21.java
@@ -2,27 +2,28 @@ import java.util.Scanner;
 public class siakad21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    String nama, nim;
-    char kelas;
+    String nama, nim, kelas;
     byte absen;
-    double nilaikuis, nilaitugas, nilaiujian, nilaiakhir;
+    double nilaikuis, nilaitugas, nilaiuts, nilaiuas, nilaiakhir;
 
     System.out.println("masukan Nama");
     nama = sc.nextLine();
     System.out.println("masukan NIM");
     nim = sc.nextLine();
     System.out.println("masukan Kelas");
-    kelas = sc.nextLine().charAt(0);
+    kelas = sc.nextLine();
     System.out.println("masukan Absen");
     absen = sc.nextByte();
     System.out.println("masukan Nilai kuis");
     nilaikuis = sc.nextDouble();
     System.out.println("masukan Nilai Tugas");
     nilaitugas = sc.nextDouble();
-    System.out.println("masukan Nilai Ujian");
-    nilaiujian = sc.nextDouble();
+    System.out.println("masukan Nilai Uts");
+    nilaiuts = sc.nextDouble();
+    System.out.println("masukan Nilai Uas");
+    nilaiuas = sc.nextDouble();
 
-    nilaiakhir = (nilaikuis + nilaitugas + nilaiujian) /3;
+    nilaiakhir = (nilaikuis*0.2 + nilaitugas*0.15 + nilaiuts*0.3 + nilaiuas*0.35);
 
     System.out.println("Mahasiswa dengan Nama " + nama + ("NIM : " + nim) + " kelas " + kelas + " Absen " + absen);
     System.out.println("nilai akhir " + nilaiakhir);
